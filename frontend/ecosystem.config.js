06027caac2d37461231ea00f12611e8cd0382564
BobrikAU <kirbob0@yandex.ru>
2023-05-08 19:57:15 +0300

    feature: change ecosystem of fontend and write url of backend

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -19,8 +19,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'pwd && cd ../source/frontend && pwd && npm -v && npm i && npm run build',
+      'post-deploy': 'cd ../source/frontend && npm i && npm run build',
     },
   },
 };
