40cb0050961cb13d5ccd132423f51eb3a998779d
BobrikAU <kirbob0@yandex.ru>
2023-05-08 14:09:46 +0300

    feature: ecosystem of backend last change

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,30 +1,27 @@
 const dotenvConfig = require('dotenv').config;
 
-dotenvConfig({ path: '.env' });
 dotenvConfig({ path: '.env.deploy' });
 
 const {
-  JWT_SECRET, DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_PATH, DEPLOY_REPO,
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_PATH, DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
   apps: [{
-    name: 'mesto-api',
-    script: 'npm run start',
-    env_production: {
-      NODE_ENV: 'production',
-      JWT_SECRET,
-    },
+    name: 'mesto-frontend',
+    // script: './src/app.ts',
+    // interpreter: './node_modules/.bin/ts-node',
   }],
   deploy: {
     production: {
+      key: '~/.ssh/id_ed25519new.pub',
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd ../source/backend && npm i && pm2 start ecosystem.config.js --env production && pm2 save',
+      'post-deploy': 'pwd && cd ../source/frontend && pwd && npm -v && npm i && npm run build',
     },
   },
 };
